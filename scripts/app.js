074fa1c66a6cd0c199f346f851eaa8b014376f7e
ASC <andreas@scherer.as>
2018-04-29 13:08:12 +0200

    test

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,11 @@
 // Copyright 2016 Google Inc.
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //      http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,6 +21,7 @@
     visibleCards: {},
     selectedCities: [],
     spinner: document.querySelector('.loader'),
+    hal9000:document.querySelector('.hal9000'),
     cardTemplate: document.querySelector('.cardTemplate'),
     container: document.querySelector('.main'),
     addDialog: document.querySelector('.dialog-container'),
