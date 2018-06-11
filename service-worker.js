90b95b64deaa5f737c214b5a29619a4859f5f067
ASC <andreas@scherer.as>
2018-06-11 15:36:43 +0200

    debug

--- service-worker.js
+++ service-worker.js
@@ -4,9 +4,9 @@ var filesToCache = [
   '/touchme/index.html',
   '/touchme/scripts/app.js',
   '/touchme/scripts/fp.js',
-  '/touchme/styles/inline.css',
+  '/touchme/styles/inline.css'/*,
   '/touchme/images/ic_add_white_24px.svg',
-  '/touchme/images/ic_refresh_white_24px.svg'
+  '/touchme/images/ic_refresh_white_24px.svg'*/
 
 ];
 
